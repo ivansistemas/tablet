c546a789cb6963c66a50b0427f152ecadc71d216
ivansistemas <79549414+ivansistemas@users.noreply.github.com>
2025-03-21 13:53:35 -0600

    version de calidad 7.4
    con transferencia a ubicacion ZONA CALIDAD

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+public class Class1
+{
+    [JsonProperty("status")]
+    public bool Status { get; set; }
+    [JsonProperty("message")]
+    public string Message { get; set; }
+    [JsonProperty("data")]
+    public Info[] Data { get; set; }
+    public Class1()
+	{
+    }
+    public class Info
+    {
+        [JsonProperty("id")] public int id { get; set; }
+        [JsonProperty("locationForValidationScan")] public string locationForValidationScan { get; set; }
+        [JsonProperty("binForValidationScan")] public string binForValidationScan { get; set; }
+        [JsonProperty("DESCRIPCION")] public string DESCRIPCION { get; set; }
+        [JsonProperty("display")] public string display { get; set; }
+
+
+
+    }
+}
